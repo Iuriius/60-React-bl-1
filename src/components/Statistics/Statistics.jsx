02670cadb58b6ef0ae63c3c62a6126af71521681
Anton Shabalov <shabalovanton@gmail.com>
2023-01-22 11:54:38 +0200

    done Task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,25 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+import PropTypes, { shape } from 'prop-types';
 
-export const Statistics = () => {
+const icons = [<FaRegThumbsUp/>, <MdPeople/>, <MdOutlineProductionQuantityLimits/>, <GiTreeDoor/>];
+
+export const Statistics = ({title, stats}) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map((stat, indx) => <StatisticItem stat ={stat} key = {stat.id} icon = {icons[indx]}/>)}
+      </StatisticsList>
     </>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(shape({
+    title: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired
+  })).isRequired
 };
